9fa84e40e7e022c3d2d3b6de56b376f093f6fa1e
ugniusnor <ugnn97@gmail.com>
2021-03-10 14:13:25 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect, useRef } from "react";
 function App() {
+  const STARTING_TIME = 5;
+  const [input, setInput] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+  const textBoxRef = useRef(null);
+
+  const handleInput = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, isTimeRunning]);
+
+  function calculateWordCount(text) {
+    const wordsArr = text
+      .trim()
+      .split(" ")
+      .filter((word) => word !== "").length;
+
+    return wordsArr;
+  }
+
+  async function startClock() {
+    await setIsTimeRunning(true);
+    setTimeRemaining(STARTING_TIME);
+    setInput("");
+
+    textBoxRef.current.focus();
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    setWordCount(calculateWordCount(input));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Title</h1>
+      <textarea
+        ref={textBoxRef}
+        onChange={(e) => handleInput(e)}
+        value={input}
+        disabled={!isTimeRunning}
+      />
+      <h4>Time remaining:{timeRemaining} </h4>
+      <button onClick={startClock} disabled={isTimeRunning}>
+        {timeRemaining !== 0 ? "Start" : "Restart"}
+      </button>
+      <h1> Word Count:{wordCount}</h1>
     </div>
   );
 }
